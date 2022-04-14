e294085d8898f06e2699ed2c79a66e0b5af2995e
Szymonexis <szymonkaszuba3@wp.pl>
2022-04-14 15:32:01 +0200

    update

--- src/main/java/com/example/conwaysgameoflifejava/MainController.java
+++ src/main/java/com/example/conwaysgameoflifejava/MainController.java
@@ -2,13 +2,17 @@ package com.example.conwaysgameoflifejava;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        ResizableCanvas canvas = new ResizableCanvas();
+        playgroundPane.getChildren().add(canvas);
+        canvas.widthProperty().bind(
+                playgroundPane.widthProperty());
+        canvas.heightProperty().bind(
+                playgroundPane.heightProperty());
     }
+    public Pane playgroundPane;
 }
